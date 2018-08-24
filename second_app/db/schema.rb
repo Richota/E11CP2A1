74922db1f998911bb28cb1c7bfa686e5a5eb8edd
RicardoSuarez <rsuarez.adv@gmail.com>
2018-08-23 20:50:40 -0400

    Se agrega email_field

--- second_app/db/schema.rb
+++ second_app/db/schema.rb
@@ -10,13 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_08_23_223455) do
+ActiveRecord::Schema.define(version: 2018_08_24_003248) do
 
   create_table "students", force: :cascade do |t|
     t.string "name"
     t.integer "age"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "email"
   end
 
 end
